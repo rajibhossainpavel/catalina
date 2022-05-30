8d85ff25e189014455a94e5b0614c683753f4a7e
rajibhossainpavel <rajibh.pavel@gmail.com>
2022-05-31 01:23:20 +0600

    initial!

--- main.go
+++ main.go
@@ -6,15 +6,30 @@ import (
 	"os"
 )
 
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
 func main() {
-	if err := os.Mkdir("data", os.ModePerm); err != nil {
-		log.Fatal(err)
+	dirERxists, _ := exists("data")
+	if !dirERxists {
+		if err := os.Mkdir("data", os.ModePerm); err != nil {
+			log.Fatal(err)
+		}
 	}
 	scrape.GetData("data/data-1.txt")
 	scrape.GetNewFile("data/data-1.txt", "data/data-2.txt", "Helpdesk for NRB")
 	scrape.GetNewFile("data/data-2.txt", "data/data-3.txt", "1JANATAMF")
 	scrape.WriteNewFile("data/data-3.txt", "data/data-4.txt", "If YCP is available")
 	scrape.WriteCSVFile("data/data-4.txt", "data.csv")
+	scrape.GetDate()
 
 	e := os.RemoveAll("data")
 	if e != nil {
--- scrape/scrape.go
+++ scrape/scrape.go
@@ -7,8 +7,9 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"unicode"
 	"time"
+	"unicode"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
@@ -18,20 +19,15 @@ func check(e error) {
 	}
 }
 func IsLetter(s string) bool {
-    for _, r := range s {
-        if unicode.IsLetter(r) {
-            return true
-        }
-    }
-    return false
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
 }
 
-func GetData(path string) {
-	handle, err := os.Create(path)
-	check(err)
-	writeBuffer := bufio.NewWriter(handle)
-	defer handle.Close()
-
+func GetDate() {
 	// Request the HTML page.
 	res, err := http.Get("https://dsebd.org/latest_share_price_scroll_l.php")
 	if err != nil {
@@ -50,31 +46,58 @@ func GetData(path string) {
 	}
 
 	// Find the review items
-	fmt.Printf("Processing Data:")
+
 	doc.Find("h2").Each(func(i int, s *goquery.Selection) {
-		targetString:=strings.ToLower(s.Text());
-		if strings.Contains(targetString, "latest share price") {
-			fmt.Println(targetString)
-			targetString=strings.ReplaceAll(targetString, "latest share price", "");
-			fmt.Println(targetString)
-			targetString=strings.ReplaceAll(targetString, "on", "");
-			fmt.Println(targetString)
-			targetString=strings.ReplaceAll(targetString, "at", "");
-			fmt.Println(targetString)
-			targetString=strings.ReplaceAll(targetString, "pm", "");
-			fmt.Println(targetString)
-			targetString=strings.TrimSpace(targetString);
-			fmt.Println(targetString)
-			date, error := time.Parse("2006-01-02", targetString)
-  
+		targetDate := strings.ToLower(s.Text())
+		if strings.Contains(targetDate, "latest share price") {
+			targetDate = strings.ReplaceAll(targetDate, "latest share price", "")
+			targetDate = strings.ReplaceAll(targetDate, "on", "")
+			//
+			//
+			targetDate = strings.TrimSpace(targetDate)
+			targetDate = strings.Title(targetDate)
+			targetDate = strings.ReplaceAll(targetDate, "At", "at")
+			targetDate = strings.ReplaceAll(targetDate, "Pm", "pm")
+			fmt.Println(targetDate)
+			//explodedDate := strings.Split(targetDate, ",")
+
+			//explodedDate = explodedDate[:len(explodedDate)-1]
+			//monthDate := strings.Join(explodedDate, "")
+			//monthDate = monthDate + ", 2022"
+			//fmt.Println(monthDate)
+
+			date, error := time.Parse(targetDate, targetDate)
+
 			if error != nil {
 				fmt.Println(error)
 				return
 			}
 			fmt.Println(date)
 		}
-	});
-	
+	})
+}
+func GetData(path string) {
+	handle, err := os.Create(path)
+	check(err)
+	writeBuffer := bufio.NewWriter(handle)
+	defer handle.Close()
+	// Request the HTML page.
+	res, err := http.Get("https://dsebd.org/latest_share_price_scroll_l.php")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Processing Data:")
 	doc.Find("tbody").Each(func(i int, s *goquery.Selection) {
 		writableString := "\n"
 		s.Find("td").Each(func(i int, s *goquery.Selection) {
@@ -176,16 +199,16 @@ func WriteCSVFile(sourcePath string, destinationPath string) bool {
 	scanner := bufio.NewScanner(sourceHandle)
 
 	line := 1
-	name:="";
+	name := ""
 	for scanner.Scan() {
-		
-		text:=strings.TrimSpace(scanner.Text());
+
+		text := strings.TrimSpace(scanner.Text())
 		if strings.HasSuffix(text, ",") {
-			name="";
-		}else if IsLetter(text){
-			name=text
-		}else{
-			_, err := writableBuffer.WriteString(name+text+ "\n")
+			name = ""
+		} else if IsLetter(text) {
+			name = text
+		} else {
+			_, err := writableBuffer.WriteString(name + text + "\n")
 			check(err)
 		}
 		line++
@@ -200,14 +223,14 @@ func WriteCSVFile(sourcePath string, destinationPath string) bool {
 }
 func main() {
 	if err := os.Mkdir("data", os.ModePerm); err != nil {
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
 	GetData("data/data-1.txt")
 	GetNewFile("data/data-1.txt", "data/data-2.txt", "Helpdesk for NRB")
 	GetNewFile("data/data-2.txt", "data/data-3.txt", "1JANATAMF")
 	WriteNewFile("data/data-3.txt", "data/data-4.txt", "If YCP is available")
 	WriteCSVFile("data/data-4.txt", "data.csv")
-	
+
 	e := os.RemoveAll("data")
 	if e != nil {
 		log.Fatal(e)
